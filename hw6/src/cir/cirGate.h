b23abe60074060fe33c48494a7a420ee9304f603
Chih-Wei Chang <jrweizhang@gmail.com>
2014-12-12 22:25:13 +0800

    Add simple Gates

--- hw6/src/cir/cirGate.h
+++ hw6/src/cir/cirGate.h
@@ -25,7 +25,9 @@ class CirGate;
 class CirGate
 {
 public:
-   CirGate() {}
+   CirGate(int id, int lineNo, int colNo, bool isInverted) :
+      _id(id), _lineNo(lineNo), _colNo(colNo), _isInverted(isInverted)
+   {}
    virtual ~CirGate() {}
 
    // Basic access methods
@@ -41,7 +43,51 @@ public:
 private:
 
 protected:
+   string           _typeStr;
 
+   vector<CirGate*> _fanins;
+   vector<CirGate*> _fanouts;
+
+   int  _id;
+   int  _lineNo;
+   int  _colNo;
+
+   bool _isInverted;
+};
+
+class CirPiGate : public CirGate {
+   CirPiGate(int id, int lineNo, int colNo, bool isInverted) :
+      CirGate(id, lineNo, colNo, isInverted) {
+         _typeStr = "PI";
+      }
+};
+
+class CirPoGate : public CirGate {
+   CirPoGate(int id, int lineNo, int colNo, bool isInverted) :
+      CirGate(id, lineNo, colNo, isInverted) {
+         _typeStr = "PO"; 
+      }
+};
+
+class CirAndGate : public CirGate {
+   CirAndGate(int id, int lineNo, int colNo, bool isInverted) :
+      CirGate(id, lineNo, colNo, isInverted) {
+         _typeStr = "AND"; 
+      }
+};
+
+class CirConstGate : public CirGate {
+   CirConstGate(int id, int lineNo, int colNo, bool isInverted) :
+      CirGate(id, lineNo, colNo, isInverted) {
+         _typeStr = "Const"; 
+      }
+};
+
+class CirUndefGate : public CirGate {
+   CirUndefGate(int id, int lineNo, int colNo, bool isInverted) :
+      CirGate(id, lineNo, colNo, isInverted) {
+         _typeStr = "UNDEF"; 
+      }
 };
 
 #endif // CIR_GATE_H
