292a9ac8510c194a8833b283171b43b327874aff
Chih-Wei Chang <jrweizhang@gmail.com>
2014-12-24 00:36:58 +0800

    Implement basic heap

--- hw7/src/util/myMinHeap.h
+++ hw7/src/util/myMinHeap.h
@@ -11,6 +11,7 @@
 
 #include <algorithm>
 #include <vector>
+#include <algorithm>
 
 template <class Data>
 class MinHeap
@@ -26,20 +27,89 @@ public:
 
    // For the following member functions,
    // We don't respond for the case vector "_data" is empty!
-   const Data& operator [] (size_t i) const { return _data[i]; }   
+   const Data& operator [] (size_t i) const { return _data[i]; }
    Data& operator [] (size_t i) { return _data[i]; }
 
    size_t size() const { return _data.size(); }
 
    // TODO
-   const Data& min() const { return Data(); }
-   void insert(const Data& d) { }
-   void delMin() { }
-   void delData(size_t i) { } // remove _data[i]
+   const Data& min() const { return _data[0]; }
+   void insert(const Data& d) {
+      _data.push_bask(d);
+
+      int idx = _data.size();
+      bool flag = true;
+
+      while (flag && idx > 0) {
+         if (_data[idx] > _data[idx/2]) {
+            flag = false;
+         } else {
+            iter_swap(_data.begin() + idx, _data.begin() + idx/2);
+            idx = idx/2;
+         }
+      }
+   }
+
+   void delMin() {
+      _data[0] = _data.back();
+      _data.pop_back();
+
+      int idx = 0;
+
+      int minVal, minIdx;
+
+      while (idx * 2 < _data.size()) {
+         if (idx * 2 + 1 < _data.size()) {
+            // Has two children
+            if (_data[idx] > _data[idx*2] || _data[idx*2+1]) {
+               // Order break
+               minVal = _data[idx];
+               minIdx = idx;
+
+               if (_data[idx*2] < minVal) {
+                  minVal = _data[idx*2];
+                  minIdx = idx*2;
+               }
+
+               if (_data[idx*2+1] < minVal) {
+                  minVal = _data[idx*2+1];
+                  minIdx = idx*2+1;
+               }
+
+               iter_swap(_data.begin() + idx, _data.begin() + minIdx);
+               idx = minIdx;
+            }
+         } else {
+            // Has one child
+            if (_data[idx] > _data[idx*2]) {
+               iter_swap(_data.begin() + idx, _data.begin() + idx*2);
+               idx = idx*2;
+            }
+         }
+      }
+   }
+
+   void delData(size_t i) {
+      _data[i] = _data[0] - 1;
+
+      int idx = i;
+      bool flag = true;
+
+      while (flag && idx > 0) {
+         if (_data[idx] > _data[idx/2]) {
+            flag = false;
+         } else {
+            iter_swap(_data.begin() + idx, _data.begin() + idx/2);
+            idx = idx/2;
+         }
+      }
+
+      delMin();
+   } // remove _data[i]
 
 private:
    // DO NOT add or change data members
-   vector<Data>   _data;
+   std::vector<Data>   _data;
 };
 
 #endif // MY_MIN_HEAP_H
