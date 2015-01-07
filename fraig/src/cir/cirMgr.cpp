5b7dc89ce294bf4b77678290c5052ed6755faab0
Chih-Wei Chang <jrweizhang@gmail.com>
2015-01-07 15:26:10 +0800

    Use my cirMgr

--- fraig/src/cir/cirMgr.cpp
+++ fraig/src/cir/cirMgr.cpp
@@ -17,6 +17,10 @@
 #include "cirGate.h"
 #include "util.h"
 
+#include <fstream>
+#include <sstream>
+#include <map>
+
 using namespace std;
 
 // TODO: Implement memeber functions for class CirMgr
@@ -152,6 +156,110 @@ parseError(CirParseError err)
 bool
 CirMgr::readCircuit(const string& fileName)
 {
+   ifstream infile(fileName.c_str());
+   istringstream iss;
+   string line;
+   string dummy; // Dump unused string
+
+   int literal;
+   int lhs, rhs_1, rhs_2; // For AND Gate
+   int lineNo = 1, colNo = 1;
+
+   int id;
+   bool isInverted;
+
+   int id_2;
+   bool isInverted_2;
+
+   getline(infile, line);
+
+   iss.str(line);
+
+   iss >> dummy; // aag
+
+   iss >> _nMax;
+   iss >> _nInput;
+
+   iss >> dummy; // n of latch
+
+   iss >> _nOutput;
+   iss >> _nAnd;
+
+   for (int i = 0; i < _nInput; ++i)
+   {
+      ++lineNo;
+      getline(infile, line);
+
+      iss.clear();
+      iss.str(line);
+      iss >> literal;
+
+      addPi(literal, lineNo, colNo);
+   }
+
+   for (int i = 0; i < _nOutput; ++i)
+   {
+      ++lineNo;
+      getline(infile, line);
+
+      iss.clear();
+      iss.str(line);
+      iss >> literal;
+
+      addPo(literal, lineNo, colNo, _nMax+i+1);
+   }
+
+   for (int i = 0; i < _nAnd; ++i)
+   {
+      ++lineNo;
+
+      getline(infile, line);
+
+      iss.clear();
+      iss.str(line);
+      iss >> lhs;
+      iss >> rhs_1;
+      iss >> rhs_2;
+
+      literal = lhs;
+
+      parseId(literal, id, isInverted);
+
+      CirAndGate* andGate = new CirAndGate(id, lineNo, colNo);
+
+      // -- Parse RHS --
+      parseId(rhs_1, id, isInverted);
+      parseId(rhs_2, id_2, isInverted_2);
+      andGate->addTmpFanin(id, isInverted, id_2, isInverted_2);
+      // ---------------
+
+      _andList.insert(pair<int, CirAndGate*>(andGate->getId(), andGate));
+   }
+
+   while (getline(infile, line) && line.size() > 0 && line.at(0) != 'c') {
+      if (line.at(0) == 'i') {
+         _piList.at(_orderedPiList.at(line.at(1) - '0'))->symbol = line.substr(3);
+      } else if (line.at(0) == 'o') {
+         _poList.at(_orderedPoList.at(line.at(1) - '0'))->symbol = line.substr(3);
+      }
+   }
+
+   // Now, start connect or set undef gates
+   for (map<int, CirPoGate*>::iterator i = _poList.begin(); i != _poList.end(); ++i) {
+      realizePoFanin(i->second);
+   }
+
+   for (map<int, CirAndGate*>::const_iterator i = _andList.begin(); i != _andList.end(); ++i) {
+      realizeAndFanin(i->second);
+   }
+
+   /* cout << "----reporting----" << endl; */
+   /* cout << "PIs: " << _piList.size() << endl; */
+   /* cout << "POs: " << _poList.size() << endl; */
+   /* cout << "AIGs: " << _andList.size() << endl; */
+   /* cout << "UNDEFs: " << _undefList.size() << endl; */
+   /* cout << "CONST:" << _constGate << endl; */
+
    return true;
 }
 
@@ -170,30 +278,139 @@ Circuit Statistics
 void
 CirMgr::printSummary() const
 {
+   cout << endl;
+   cout << "Circuit Statistics" << endl;
+   cout << "==================" << endl;
+
+   cout << "  PI";
+   cout.width(12);
+   cout << right << _piList.size() << endl;
+
+   cout << "  PO";
+   cout.width(12);
+   cout << right <<  _poList.size() << endl;
+
+   cout << "  AIG";
+   cout.width(11);
+   cout << right <<  _andList.size() << endl;
+   cout << "------------------" << endl;
+
+   cout << "  Total";
+   cout.width(9);
+   cout << right << (_piList.size() + _poList.size() + _andList.size()) << endl;
 }
 
 void
 CirMgr::printNetlist() const
 {
+   CirGate::setGlobalRef();
+   _dfsIdList.clear();
+
+   CirGate* toPrint;
+   int counter = 0;
+
+   cout << endl;
+
+   for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+      getGate(*i)->dfsTraversal(_dfsIdList);
+   }
+
+   for (vector<int>::const_iterator i = _dfsIdList.begin(); i != _dfsIdList.end(); ++i) {
+      toPrint = getGate(*i);
+      cout << "[" << counter++ << "] ";
+      toPrint->printGate();
+   }
 }
+
 
 void
 CirMgr::printPIs() const
 {
-   cout << "PIs of the circuit:";
+   cout << "PIs of the circuit: ";
+
+   for (vector<int>::const_iterator i = _orderedPiList.begin(); i != _orderedPiList.end(); ++i) {
+      cout << *i;
+      if (i+1 != _orderedPiList.end()) {
+         cout << " ";
+      }
+   }
+
    cout << endl;
 }
 
 void
 CirMgr::printPOs() const
 {
-   cout << "POs of the circuit:";
+   cout << "POs of the circuit: ";
+
+   for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+      cout << *i;
+      if (i+1 != _orderedPoList.end()) {
+         cout << " ";
+      }
+   }
+
    cout << endl;
 }
 
 void
 CirMgr::printFloatGates() const
 {
+   vector<int> ids;
+
+   // floating -- fanin not define
+
+   for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+      if (getGate(*i)->isFloating()) {
+         ids.push_back(*i);
+      }
+   }
+
+   for (map<int, CirAndGate*>::const_iterator i = _andList.begin(); i != _andList.end(); ++i) {
+      if (i->second->isFloating()) {
+         ids.push_back(i->first);
+      }
+   }
+
+   if (!ids.empty()) {
+      sort(ids.begin(), ids.end());
+      cout << "Gates with floating fanin(s): ";
+      for (vector<int>::const_iterator i = ids.begin(); i != ids.end(); ++i) {
+         cout << *i;
+         if (i+1 != ids.end()) {
+            cout << " ";
+         }
+      }
+      cout << endl;
+   }
+
+   ids.clear();
+
+   // define but not used -- no fanouts
+
+   for (vector<int>::const_iterator i = _orderedPiList.begin(); i != _orderedPiList.end(); ++i) {
+      if (getGate(*i)->getFanoutList().empty()) {
+         ids.push_back(*i);
+      }
+   }
+
+   for (map<int, CirAndGate*>::const_iterator i = _andList.begin(); i != _andList.end(); ++i) {
+      if (i->second->getFanoutList().empty()) {
+         ids.push_back(i->first);
+      }
+   }
+
+   if (!ids.empty()) {
+      sort(ids.begin(), ids.end());
+      cout << "Gates defined but not used  : ";
+      for (vector<int>::const_iterator i = ids.begin(); i != ids.end(); ++i) {
+         cout << *i;
+         if (i+1 != ids.end()) {
+            cout << " ";
+         }
+      }
+      cout << endl;
+   }
 }
 
 void
@@ -204,5 +421,184 @@ CirMgr::printFECPairs() const
 void
 CirMgr::writeAag(ostream& outfile) const
 {
+   CirGate* targetGate;
+   vector<GateIdInv>::const_iterator targetFanin;
+
+   int symCount;
+
+   if (_dfsIdList.empty()) {
+      CirGate::setGlobalRef();
+
+      for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+         getGate(*i)->dfsTraversal(_dfsIdList);
+      }
+   }
+
+   outfile << "aag "
+      << _nMax << " "
+      << _nInput << " 0 "
+      << _nOutput << " "
+      << _nAnd << endl;
+
+   for (vector<int>::const_iterator i = _orderedPiList.begin(); i != _orderedPiList.end(); ++i) {
+      outfile << 2*(*i) << endl;
+   }
+
+   for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+      targetFanin = getGate(*i)->getFaninList().begin();
+
+      if (targetFanin->second) {
+         outfile << 2*(targetFanin->first) + 1 << endl;
+      } else {
+         outfile << 2*(targetFanin->first) << endl;
+      }
+   }
+
+   for (vector<int>::const_iterator i = _dfsIdList.begin(); i != _dfsIdList.end(); ++i) {
+      if (_andList.count(*i)) {
+         targetGate = _andList.at(*i);
+
+         vector<GateIdInv>::const_iterator fanin = targetGate->getFaninList().begin();
+
+         outfile << 2*(*i) << " ";
+
+         if (fanin->second) {
+            outfile << 2*(fanin->first)+1;
+         } else {
+            outfile << 2*(fanin->first);
+         }
+         outfile << " ";
+
+         ++fanin;
+
+         if (fanin->second) {
+            outfile << 2*(fanin->first)+1;
+         } else {
+            outfile << 2*(fanin->first);
+         }
+         outfile << endl;
+      }
+   }
+
+   symCount = 0;
+
+   for (vector<int>::const_iterator i = _orderedPiList.begin(); i != _orderedPiList.end(); ++i) {
+      targetGate = _piList.at(*i);
+      if (targetGate->symbol.size() > 0) {
+         outfile << "i" << symCount << " " << targetGate->symbol << endl;
+      }
+      ++symCount;
+   }
+
+   symCount = 0;
+
+   for (vector<int>::const_iterator i = _orderedPoList.begin(); i != _orderedPoList.end(); ++i) {
+      targetGate = _poList.at(*i);
+      if (targetGate->symbol.size() > 0) {
+         outfile << "o" << symCount << " " << targetGate->symbol << endl;
+      }
+      ++symCount;
+   }
 }
 
+/**********************************************************/
+/*   class CirMgr helper member functions                 */
+/**********************************************************/
+void
+CirMgr::parseId(const int& literal, int& id, bool& isInverted)
+{
+   assert (literal >= 0);
+
+   if (literal % 2 == 0) {
+      id = literal / 2;
+      isInverted = false;
+   } else {
+      id = (literal - 1) / 2;
+      isInverted = true;
+   }
+}
+
+void
+CirMgr::realizePoFanin(CirPoGate* poGate)
+{
+   int id;
+   bool isInverted;
+
+   CirGate* fanin;
+
+   poGate->getTmpFanin(id, isInverted);
+
+   if (id != -1) {
+      if (id == 0 || id == 1) {
+         createConstGate();
+      }
+
+      fanin = getGateInAll(id);
+      if (fanin == 0) {
+         fanin = new CirUndefGate(id, -1, -1);
+         _undefList.insert(pair<int, CirUndefGate*>(fanin->getId(), (CirUndefGate*) fanin));
+      }
+
+      poGate->addFanin(fanin, isInverted);
+   }
+}
+
+void
+CirMgr::realizeAndFanin(CirAndGate* andGate)
+{
+   int id_1;
+   bool isInverted_1;
+
+   int id_2;
+   bool isInverted_2;
+
+   CirGate* fanin;
+
+   andGate->getTmpFanin(id_1, isInverted_1, id_2, isInverted_2);
+
+   if (id_1 != -1) {
+      if (id_1 == 0 || id_1 == 1) {
+         createConstGate();
+      }
+
+      fanin = getGateInAll(id_1);
+      if (fanin == 0) {
+         fanin = new CirUndefGate(id_1, -1, -1);
+         _undefList.insert(pair<int, CirUndefGate*>(fanin->getId(), (CirUndefGate*) fanin));
+      }
+
+      andGate->addFanin(fanin, isInverted_1);
+   }
+
+   if (id_2 != -1) {
+      if (id_2 == 0 || id_2 == 1) {
+         createConstGate();
+      }
+
+      fanin = getGateInAll(id_2);
+      if (fanin == 0) {
+         fanin = new CirUndefGate(id_2, -1, -1);
+         _undefList.insert(pair<int, CirUndefGate*>(fanin->getId(), (CirUndefGate*) fanin));
+      }
+
+      andGate->addFanin(getGateInAll(id_2), isInverted_2);
+   }
+}
+
+CirGate*
+CirMgr::getGateInAll(const int& id) const
+{
+   if (_piList.count(id)) {
+      return _piList.at(id);
+   } else if (_poList.count(id)) {
+      return _poList.at(id);
+   } else if (_andList.count(id)) {
+      return _andList.at(id);
+   } else if (_undefList.count(id)) {
+      return _undefList.at(id);
+   } else if (id == 0) {
+      return getConstGate();
+   } else {
+      return 0;
+   }
+}
--- fraig/src/cir/cirMgr.h
+++ fraig/src/cir/cirMgr.h
@@ -13,24 +13,34 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
-// TODO: Feel free to define your own classes, variables, or functions.
-
 #include "cirDef.h"
+#include "cirGate.h"
 
 extern CirMgr *cirMgr;
 
+// TODO: Define your own data members and member functions
 class CirMgr
 {
 public:
-   CirMgr() {}
+   CirMgr() : _constGate(0) {}
    ~CirMgr() {}
 
    // Access functions
    // return '0' if "gid" corresponds to an undefined gate.
-   CirGate* getGate(unsigned gid) const { return 0; }
+   CirGate* getGate(unsigned gid) const {
+      if (_undefList.count(gid)) {
+         return 0;
+      } else {
+         return getGateInAll(gid);
+      }
+   }
+
+   // This returns including UNDEF gates.
+   CirGate* getGateInAll(const int&) const;
 
    // Member functions about circuit construction
    bool readCircuit(const string&);
@@ -58,9 +68,53 @@ public:
    void printFECPairs() const;
    void writeAag(ostream&) const;
 
+   void createConstGate() {
+      if (_constGate == 0) {
+         _constGate = new CirConstGate(0, 0, 0);
+      }
+   }
+
+   CirConstGate* getConstGate() const { return _constGate; }
+
 private:
    ofstream           *_simLog;
+   void parseId(const int&, int&, bool&);
+   void realizePoFanin(CirPoGate*);
+   void realizeAndFanin(CirAndGate*);
 
+   void addPi(const int& literal, const int& lineNo, const int& colNo) {
+      int id;
+      bool isInverted; // dummy bool
+      parseId(literal, id, isInverted);
+      _piList.insert(pair<int, CirPiGate*>(id, new CirPiGate(id, lineNo, colNo)));
+      _orderedPiList.push_back(id);
+   }
+
+   void addPo(const int& literal, const int& lineNo, const int& colNo, const int& poId) {
+      int id;
+      bool isInverted;
+      parseId(literal, id, isInverted);
+
+      CirPoGate* poGate = new CirPoGate(poId, lineNo, colNo);
+      poGate->addTmpFanin(id, isInverted);
+
+      _poList.insert(pair<int, CirPoGate*>(poGate->getId(), poGate));
+      _orderedPoList.push_back(poGate->getId());
+   }
+
+   mutable vector<int> _dfsIdList;
+
+   map<int, CirPiGate*>    _piList;
+   map<int, CirPoGate*>    _poList;
+   map<int, CirAndGate*>   _andList;
+   map<int, CirUndefGate*> _undefList;
+
+   vector<int> _orderedPiList;
+   vector<int> _orderedPoList;
+
+   CirConstGate* _constGate;
+
+   int _nMax, _nInput, _nOutput, _nAnd;
 };
 
 #endif // CIR_MGR_H
