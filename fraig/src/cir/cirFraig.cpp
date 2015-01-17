7c1eb0248cea8b3409f86f36753a567f9d41c906
Chih-Wei Chang <jrweizhang@gmail.com>
2015-01-17 14:35:19 +0800

    Compare inv stage as well

--- fraig/src/cir/cirFraig.cpp
+++ fraig/src/cir/cirFraig.cpp
@@ -19,27 +19,30 @@ class myHashKey
 {
 public:
   myHashKey(CirGate* g) {
-    _faninID1 = g->getFanin(0).gate()->getId();
-    _faninID2 = g->getFanin(1).gate()->getId();
+    _fanin1 = g->getFanin(0);
+    _fanin2 = g->getFanin(1);
   }
-  myHashKey() : _faninID1(0), _faninID2(0) {}
 
   // Ref: http://stackoverflow.com/a/919661/1371471
   size_t operator() () const {
-    size_t tmp1 = (_faninID1 + _faninID2) % 1119;
-    size_t tmp2 = (_faninID1 + _faninID2 + 1) % 1119;
+    size_t tmp1 = (_fanin1.gate()->getId() + _fanin2.gate()->getId()) % 1119;
+    size_t tmp2 = (_fanin1.gate()->getId() + _fanin2.gate()->getId() + 1) % 1119;
     size_t res = (tmp1 * tmp2) % 1119;
     return  res;
   }
 
   bool operator == (const myHashKey& k) const {
-    return ((k._faninID1 == _faninID1) && (k._faninID2 == _faninID2)) ||
-            ((k._faninID1 == _faninID2) && (k._faninID2 == _faninID1));
+    return (isSameAGV(k._fanin1, _fanin1) && isSameAGV(k._fanin2, _fanin2)) ||
+            (isSameAGV(k._fanin1, _fanin2) && isSameAGV(k._fanin2, _fanin1));
   }
 
 private:
-  size_t _faninID1;
-  size_t _faninID2;
+  AigGateV _fanin1;
+  AigGateV _fanin2;
+
+  bool isSameAGV(const AigGateV& agv1, const AigGateV& agv2) const {
+    return (agv1.gate()->getId() == agv2.gate()->getId()) && (agv1.isInv() == agv2.isInv());
+  }
 };
 
 // TODO: Please keep "CirMgr::strash()" and "CirMgr::fraig()" for cir cmd.
@@ -77,6 +80,7 @@ CirMgr::strash()
 
       AigGateV newFanin(existedGate, 0);
       gate->connectFaninToEachFanout(newFanin);
+      _aigs.erase(remove(_aigs.begin(), _aigs.end(), gate), _aigs.end());
 
       _dfsOrder.erase(_dfsOrder.begin() + i);
       --i, --dfsSize;
@@ -86,6 +90,9 @@ CirMgr::strash()
       myHash.insert(key, gate);
     }
   }
+
+  _dfsOrder.clear();
+  topoSort();
 }
 
 void
